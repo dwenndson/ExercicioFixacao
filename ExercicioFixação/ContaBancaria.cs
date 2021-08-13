ced0c9f6d3cd78d60ceb95926cc8b2811395ea76
dwenndson <diego.wenndson@gmail.com>
2021-08-13 10:18:23 -0300

    subindo exercicio de fixacao

---
+++ ExercicioFixação/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixação
+{
+    class ContaBancaria
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; private set; }
+
+        public ContaBancaria() { }
+        public ContaBancaria (int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int conta, string titular, decimal saldo) : this (conta, titular)
+        {
+            Deposito(saldo);
+        }
+
+        public void Deposito(decimal deposito)
+        {
+            Saldo += deposito;
+            
+        }
+
+        public void Saque(decimal saque)
+        {
+            decimal taxa = 5;
+            Saldo -= taxa;
+            Saldo -= saque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Conta + ", Titular: " + Titular + ", Saldo: R$" + Saldo.ToString("F2");
+        }
+    }
+}
---
+++ ExercicioFixação/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ExercicioFixação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta;
+            Console.Write("Entre o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char res = char.Parse(Console.ReadLine());
+            if(res.Equals('s')|| res.Equals('S'))
+            {
+                decimal deposito = decimal.Parse(Console.ReadLine());
+                conta = new ContaBancaria( numero, titular, deposito);
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta bancaria");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja depositar na conta?");
+            res = char.Parse(Console.ReadLine());
+            if(res == 's' || res == 'S')
+            {
+                Console.WriteLine("Valor a depositar: ");
+                decimal dp = decimal.Parse(Console.ReadLine());
+                conta.Deposito(dp);
+                Console.WriteLine(conta);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja fazer um saque? ");
+            res = char.Parse(Console.ReadLine());
+            if (res.Equals('s') || res.Equals('S')){
+                Console.Write("Valor do Saque: R$");
+                decimal sq = decimal.Parse(Console.ReadLine());
+                conta.Saque(sq);
+                Console.WriteLine(conta);
+
+            }
+
+        }
+    }
+}
