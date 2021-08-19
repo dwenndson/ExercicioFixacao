3a1397e4a8021a7a0bf1222ae279c813bf7cb287
dwenndson <diego.wenndson@gmail.com>
2021-08-19 17:02:41 -0300

    continuação dos estudos

---
+++ estudoFixacaoMatriz/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace estudoFixacaoMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
+
+            int[,] matriz = new int[m, n];
+            for(int i = 0; i < m; i++)
+            {
+                Console.WriteLine("Divida os valores da matriz, com espaços: ");
+                line = Console.ReadLine().Split(' ');
+                for(int j = 0; j < n; j++)
+                {
+                    matriz[i,j] = int.Parse(line[j]);
+                }
+            }
+            Console.WriteLine();
+            Console.Write("Digite o valor que deseja procurar: ");
+            int x  = int.Parse(Console.ReadLine());
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + matriz[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + matriz[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + matriz[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + matriz[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
